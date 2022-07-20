a707cc86d17732aad4649a2690fb68ec463e0c7e
“TheWeion” <ianterryfallows@gmail.com>
2022-07-20 15:31:16 +0100

    Revert

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-
-
-const PORT = process.env.PORT || 8000;
-const INDEX = '/index.html';
+const app = require("express")();
+const server = require("http").createServer(app);
 const io = require("socket.io")(server); // integrate our http server with a new instance of socket.io
 const gameLogic = require("./game-logic");
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 // socket connection will go here
 
 io.on("connection", client => {
   gameLogic.initaliseGame(io, client);
 });
 
-server.listen(process.env.PORT || 8000);
+server.listen(process.env.PORT || 8000); 
