b1ae5cd3be56a2a546a2377a31f2191530b32cc9
“TheWeion” <ianterryfallows@gmail.com>
2022-07-20 15:12:58 +0100

    Modified to Test Heroku handshake.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server); // integrate our http server with a new instance of socket.io
+
+
+const PORT = process.env.PORT || 8000;
+const INDEX = '/index.html';
+const io = socketIO(server); // integrate our http server with a new instance of socket.io
 const gameLogic = require("./game-logic");
 
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 // socket connection will go here
 
 io.on("connection", client => {
