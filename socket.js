50e855c476c7fd2eca41722e75eef9dfeba5a12a
wingyuen2022 <wingyuenuk330@gmail.com>
2022-07-21 14:04:40 +0100

    Removed * inside cors()

--- socket.js
+++ socket.js
@@ -7,7 +7,8 @@ const gameLogic = require("./game-logic");
 
 // socket connection will go here
 
-app.use(cors('*'));
+//app.use(cors('*'));
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
